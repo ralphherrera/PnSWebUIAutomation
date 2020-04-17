39015c7e4740da319c7666fcd355dcbad21d0e5d
ralphherrera <ralphpauloherrera@gmail.com>
2020-04-17 17:07:29 +0800

    Added / updated the follwing:
    1) Added plugin configuration to exlclude main when compiling the project via Maven
    2) Added Steps Duration capture for the ApplyLearnerOnVFHCourseTest
    
    Signed-off-by: ralphherrera <ralphpauloherrera@gmail.com>

--- src/test/java/com/pnswebuiautomation/testcases/EnquireLearnerOnVFHCourseTest.java
+++ src/test/java/com/pnswebuiautomation/testcases/EnquireLearnerOnVFHCourseTest.java
@@ -1,28 +1,42 @@
 package com.pnswebuiautomation.testcases;
 
 import com.pnswebuiautomation.pageobjects.*;
+import com.pnswebuiautomation.utilities.CommonUtility;
 import com.pnswebuiautomation.utilities.FileMgmtUtility;
 import com.pnswebuiautomation.utilities.WebActionsUtility;
 import com.pnswebuiautomation.utilities.WebWaitsUtility;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
-public class ApplyLearnerOnVFHCourseTest {
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
-    private static final Logger log = LogManager.getLogger(ApplyLearnerOnVFHCourseTest.class);
+public class EnquireLearnerOnVFHCourseTest {
+
+    private static final Logger log = LogManager.getLogger(EnquireLearnerOnVFHCourseTest.class);
 
     private WebActionsUtility webActionsUtil;
     private WebWaitsUtility webWaitsUtil;
+    private LocalDateTime stepStartTime;
+    private LocalDateTime stepEndTime;
+    private static List<String> stepDurationList = new ArrayList<>();
+    private static final String TEST_CASE_NAME = "Enquire Learner on VFH Course";
 
     private static final int RUN_DURATION = FileMgmtUtility.getNumberValue("run.minute");
     private final long NANOSEC_PER_SEC = 1000l*1000*1000;
 
+    @BeforeEach
+    public void setUpBeforeEachTest() {
+
+    }
+
+
     @Test
     @Tag("webTest")
-    public void ApplyLearnerOnVFHCourseTest() {
+    public void EnquireLearnerOnVFHCourseTest() {
         long startTime = System.nanoTime();
         while ((System.nanoTime()-startTime)< RUN_DURATION * 60 * NANOSEC_PER_SEC){
 
@@ -47,19 +61,32 @@ public class ApplyLearnerOnVFHCourseTest {
             reviewPersonalInformationPage.setDriver(webWaitsUtil, webActionsUtil);
             confirmationOfApplicationPage.setDriver(webWaitsUtil, webActionsUtil);
 
+            stepStartTime = CommonUtility.getCurrentTimeStamp();
             getStartedPage.openTAFEEnrolmentPortalSite(FileMgmtUtility.getPropertyValue("website.url"));
+            stepEndTime = CommonUtility.getCurrentTimeStamp();
+            stepDurationList.add(CommonUtility.processStepDuration(stepStartTime, stepEndTime, TEST_CASE_NAME,
+                    "Step 1"));
 
+            stepStartTime = CommonUtility.getCurrentTimeStamp();
             getStartedPage.clickElementFromList("HowCourseSourceList", "Google");
             getStartedPage.clickElementFromList("StudiedPreviouslyList", "No");
             getStartedPage.clickElement("Continue button");
+            stepEndTime = CommonUtility.getCurrentTimeStamp();
+            stepDurationList.add(CommonUtility.processStepDuration(stepStartTime, stepEndTime, TEST_CASE_NAME,
+                    "Step 2"));
 
+            stepStartTime = CommonUtility.getCurrentTimeStamp();
             aboutYouPage.clickElementFromList("ResideInAustraliaList", "Yes");
             aboutYouPage.clickElementFromList("AreYouAUSResidentList", "Permanent Humanitarian visa holder");
             aboutYouPage.inputTextToField("UniqueStudentIdTxtField", "HE73375392");
             aboutYouPage.clickElementFromList("IndigenousStatusList", "No");
             aboutYouPage.clickElementFromList("ApprenticeshipList", "Neither");
             aboutYouPage.clickElement("ContinuePage1of3 button");
+            stepEndTime = CommonUtility.getCurrentTimeStamp();
+            stepDurationList.add(CommonUtility.processStepDuration(stepStartTime, stepEndTime, TEST_CASE_NAME,
+                    "Step 2"));
 
+            stepStartTime = CommonUtility.getCurrentTimeStamp();
             aboutYouPage.clickElementFromList("PersonTitleList", "Reverend");
             aboutYouPage.inputTextToField("FamilyNameTxtField", "Scotch");
             aboutYouPage.inputTextToField("FirstNameTxtField", "Tafe");
@@ -68,12 +95,24 @@ public class ApplyLearnerOnVFHCourseTest {
             aboutYouPage.inputTextToField("EmailAddressTxtField", "scotch.tafe@mail.com");
             aboutYouPage.inputTextToField("ConfirmEmailAddressTxtField", "scotch.tafe@mail.com");
             aboutYouPage.clickElement("ContinuePage2of3 button");
+            stepEndTime = CommonUtility.getCurrentTimeStamp();
+            stepDurationList.add(CommonUtility.processStepDuration(stepStartTime, stepEndTime, TEST_CASE_NAME,
+                    "Step 3"));
 
+            stepStartTime = CommonUtility.getCurrentTimeStamp();
             usiVerificationUnsuccessfulPage.clickElement("Continue Button");
+            stepEndTime = CommonUtility.getCurrentTimeStamp();
+            stepDurationList.add(CommonUtility.processStepDuration(stepStartTime, stepEndTime, TEST_CASE_NAME,
+                    "Step 4"));
 
+            stepStartTime = CommonUtility.getCurrentTimeStamp();
             aboutYouCheckAndConfirmPage.clickElement("ConfirmDetails");
             aboutYouCheckAndConfirmPage.clickElement("Continue Button");
+            stepEndTime = CommonUtility.getCurrentTimeStamp();
+            stepDurationList.add(CommonUtility.processStepDuration(stepStartTime, stepEndTime, TEST_CASE_NAME,
+                    "Step 5"));
 
+            stepStartTime = CommonUtility.getCurrentTimeStamp();
             aboutYouPage.inputTextToField("StreetResidentialAddressLine", "Lo");
             aboutYouPage.inputTextToField("ResidentialAddressPostCode", "2000");
             aboutYouPage.clickElement("ValidateAddressPostalCode");
@@ -82,20 +121,54 @@ public class ApplyLearnerOnVFHCourseTest {
             aboutYouPage.clickElementFromList("SpeakOtherLanguagesAtHomeList", "No");
             aboutYouPage.clickElementFromList("DisabilityStatusList", "No, I don't have a disability");
             aboutYouPage.clickElement("ContinuePage3of3 button");
+            stepEndTime = CommonUtility.getCurrentTimeStamp();
+            stepDurationList.add(CommonUtility.processStepDuration(stepStartTime, stepEndTime, TEST_CASE_NAME,
+                    "Step 6"));
 
+            stepStartTime = CommonUtility.getCurrentTimeStamp();
             qualificationsPage.clickElementFromList("CountryOfBirthList", "Australia");
             qualificationsPage.clickElementFromList("CurrentEmploymentStatusList", "Part-time employee");
             qualificationsPage.clickElementFromList("ReasonForTakingList", "To try for a different career");
             qualificationsPage.clickElement("Continue Button");
+            stepEndTime = CommonUtility.getCurrentTimeStamp();
+            stepDurationList.add(CommonUtility.processStepDuration(stepStartTime, stepEndTime, TEST_CASE_NAME,
+                    "Step 7"));
 
+            stepStartTime = CommonUtility.getCurrentTimeStamp();
             reviewPersonalInformationPage.clickElement("ConfirmDetails");
             reviewPersonalInformationPage.clickElement("Continue Button");
+            stepEndTime = CommonUtility.getCurrentTimeStamp();
+            stepDurationList.add(CommonUtility.processStepDuration(stepStartTime, stepEndTime, TEST_CASE_NAME,
+                    "Step8"));
 
+            stepStartTime = CommonUtility.getCurrentTimeStamp();
             Assertions.assertTrue(confirmationOfApplicationPage.isElementDisplayed("Header Text"),
                     "Verify Application of Learner to VFH Course is Successful");
+            stepEndTime = CommonUtility.getCurrentTimeStamp();
+            stepDurationList.add(CommonUtility.processStepDuration(stepStartTime, stepEndTime, TEST_CASE_NAME,
+                    "Step9"));
 
             webDriverBuilder.tearDownWebDriver();
         }
+    }
+
+    @AfterAll
+    public static void tearDownAfterAllTest() {
+        final String CSV_FILE_EXT = ".csv";
+        final String PROJECT_DIR_PATH = System.getProperty("user.dir");
+
+        StringBuilder fileNameToPath = new StringBuilder();
+        fileNameToPath.append(PROJECT_DIR_PATH);
+        fileNameToPath.append("/");
+        fileNameToPath.append("Results");
+        fileNameToPath.append("/");
+        fileNameToPath.append(TEST_CASE_NAME.replaceAll(" ", "_"));
+        fileNameToPath.append("-");
+        final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yy-MM-dd_HH-mm-ss");
+        fileNameToPath.append(LocalDateTime.now().format(DATE_TIME_FORMATTER));
+        fileNameToPath.append(CSV_FILE_EXT);
+
+        FileMgmtUtility.writeTestStepDurationToCSV(fileNameToPath.toString(), stepDurationList);
 
     }
 }
